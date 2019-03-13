a700c05ec4943990a454cc8d1df94b33706a1f3e
skooter500 <skooter500@gmail.com>
2019-03-13 17:41:07 +0000

    Added P3D libraries

--- java/src/ie/tudublin/UI.java
+++ java/src/ie/tudublin/UI.java
@@ -27,7 +27,7 @@ public class UI extends PApplet
 
     public void settings()
     {
-        size(800, 800);
+        size(800, 800, P3D);
         // Use fullscreen instead of size to make your interface fullscreen
         //fullScreen(); 
     }
