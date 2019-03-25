6183a03d3f154d52f0005cf6e27557a0bb9e247d
Arkadiusz Rybicki <C17337523@mydit.ie>
2019-03-25 17:24:38 +0000

    sunny

--- java/src/ie/tudublin/Sun.java
+++ java/src/ie/tudublin/Sun.java
@@ -11,27 +11,21 @@ public class Sun
     private float diameter;
     PApplet ui;
     PImage sunny;
-
-    
-
-    
-
+	
     public Sun(PApplet ui, float x, float y, float diameter)
     {
         this.ui = ui;
         this.x = x;
         this.y = y;
-        this.diameter = diameter;
-               
-    }
-    
+        this.diameter = diameter;               
+    }    
     
     public void render()
-    {
-        
-        ui.stroke(255,154,0);
-        
-        ui.ellipse(x, y , diameter, diameter);                           
+    {        
+        ui.stroke(255,154,0);        
+        ui.ellipse(x, y , diameter, diameter);   
+		ui.fill(255,0,0);
+		ui.ellipse(x, y , diameter - 5, diameter - 5); 
     }
 
     // public void draw()
