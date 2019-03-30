4a72fcf3505c85d27d2f2c5dc984d279704341e0
unknown <zysio1998@gmail.com>
2019-03-30 00:43:15 +0000

    3rd planet

--- java/src/ie/tudublin/Sun.java
+++ java/src/ie/tudublin/Sun.java
@@ -10,7 +10,7 @@ public class Sun
     private float y;
     private float diameter;
     PApplet ui;
-	PImage sunny;
+	PImage sunny,mars;
 	
 		
     public Sun(PApplet ui, float x, float y, float diameter)
@@ -19,7 +19,8 @@ public class Sun
         this.x = x;
         this.y = y;
         this.diameter = diameter;     
-		sunny = ui.loadImage("images/sun.png");
+        sunny = ui.loadImage("images/sun.png");
+        mars = ui.loadImage("images/sun.png");
 		
     }    
 	
@@ -27,6 +28,7 @@ public class Sun
     public void render()
     {        
         ui.image(sunny,x - 50,y - 50);
+        ui.image(mars,400,750);
     }
 	
 }
