4a0464f75ccf1249e7fe5237987952b32ecd8dde
Arkadiusz Rybicki <C17337523@mydit.ie>
2019-04-01 18:28:35 +0100

    mars

--- java/src/ie/tudublin/Sun.java
+++ java/src/ie/tudublin/Sun.java
@@ -10,7 +10,7 @@ public class Sun
     private float y;
     private float diameter;
     PApplet ui;
-	PImage sunny,jupiter;
+	PImage sunny,jupiter,mars;
 	
 		
     public Sun(PApplet ui, float x, float y, float diameter)
@@ -21,6 +21,7 @@ public class Sun
         this.diameter = diameter;     
         sunny = ui.loadImage("images/sunny.png");
         jupiter = ui.loadImage("images/jupitr.png");
+		mars = ui.loadImage("images/mars.png");
 		
     }    
 	
@@ -28,7 +29,8 @@ public class Sun
     public void render()
     {        
         ui.image(sunny,x - 75,y - 75);
-        ui.image(jupiter,x - 75,y - 75);
+        ui.image(jupiter,x -300 ,y -37 );  
+		ui.image(mars,x -220 ,y -20);
     }
 	
 }
