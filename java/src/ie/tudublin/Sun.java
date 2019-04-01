81dd0505f87103cac8925bb7dfdd86487bbb5966
Arkadiusz Rybicki <C17337523@mydit.ie>
2019-04-01 20:13:48 +0100

    Saturn

--- java/src/ie/tudublin/Sun.java
+++ java/src/ie/tudublin/Sun.java
@@ -10,9 +10,9 @@ public class Sun
     private float y;
     private float diameter;
     PApplet ui;
-	PImage sunny,jupiter,mars,venus,mercury;
+	PImage sun,mercury,venus,earth,mars,jupiter,saturn;
 	
-	PImage earth;
+	
 	
 		
     public Sun(PApplet ui, float x, float y, float diameter)
@@ -21,24 +21,30 @@ public class Sun
         this.x = x;
         this.y = y;
         this.diameter = diameter;     
-        sunny = ui.loadImage("images/sunny.png"); //150*150
-        jupiter = ui.loadImage("images/jupiter.png"); //75*75
-		mars = ui.loadImage("images/mars.png"); //30*30
-		venus = ui.loadImage("images/venus.png"); //40*40
-		mercury = ui.loadImage("images/mercury.png"); //25*25
+        sun = ui.loadImage("images/sun.png"); //150*150
+        mercury = ui.loadImage("images/mercury.png"); //25*25		
+		venus = ui.loadImage("images/venus.png"); //40*40		
 		earth = ui.loadImage("images/earth.png"); //40*40
+		mars = ui.loadImage("images/mars.png"); //30*30
+		jupiter = ui.loadImage("images/jupiter.png"); //75*75
+		saturn = ui.loadImage("images/saturn.png"); //60*60
 		
     }    
 	
     
     public void render()
     {        
-        ui.image(sunny,x - 75,y - 75);
-        ui.image(jupiter,x -330 ,y -37 );  
-		ui.image(mars,x -250 ,y -15);
-		ui.image(venus,x -170 ,y -20);
-		ui.image(mercury,x -125 ,y -12);
-		ui.image(earth,x -215 ,y -20);
+        ui.image(sun,x - 75,y - 75);
+		ui.image(mercury,x -110 ,y -12);
+		ui.image(venus,x -155 ,y -20);
+		ui.image(earth,x -200 ,y -20);
+		ui.image(mars,x -235 ,y -15);		
+        ui.image(jupiter,x -315 ,y -37 );  
+		ui.image(saturn,x -380 ,y -30 );
+		
+		
+		
+		
     }
 	
 }
