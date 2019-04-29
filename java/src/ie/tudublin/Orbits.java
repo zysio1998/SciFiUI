de18057321e63d7c2456b727d92af6cb3905526f
Robert Rybicki <zysio1998@gmail.com>
2019-04-29 19:06:07 +0100

    adding a new class 'Zooming'

--- java/src/ie/tudublin/Orbits.java
+++ java/src/ie/tudublin/Orbits.java
@@ -1,22 +1,14 @@
 package ie.tudublin;
 
-import static processing.core.PConstants.SPHERE;
-
 import processing.core.PApplet;
 import processing.core.PImage;
-import processing.core.PShape;
 
 public class Orbits extends PlanetObjects {
 
     private float MercuryAngle, VenusAngle, EarthAngle, MarsAngle, JupiterAngle, SaturnAngle, UranusAngle, NeptuneAngle;
-    private PImage sun, mercury, venus, earth, mars, jupiter, saturn, uranus, neptune;
-    private float radius;
-    private PImage img;
-    private PShape globe;
-    private PShape globe2;
+    private PImage sun, mercury, venus, earth, mars, jupiter, saturn, uranus, neptune;    
     private float halfWidth;
-    private float halfHeight;
-    private PImage[] textures = new PImage[8]; 
+    private float halfHeight;    
 
     public Orbits(PApplet ui, float x, float y, float width,float height) {
         super(ui, x, y, width,height);
@@ -29,44 +21,15 @@ public class Orbits extends PlanetObjects {
         jupiter = ui.loadImage("images/jupiter.png"); // 70*70
         saturn = ui.loadImage("images/saturn.png"); // 55*55
         uranus = ui.loadImage("images/uranus.png"); // 35*35
-        neptune = ui.loadImage("images/neptune.png"); // 35*35
-
-        img = ui.loadImage("images/sun.jpg");
-        textures[0] = ui.loadImage("images/mercury.jpg");
-        textures[1] = ui.loadImage("images/venus.jpg");
-        textures[2] = ui.loadImage("images/earth.jpg");
-        textures[3] = ui.loadImage("images/mars.jpg");
-        textures[4] = ui.loadImage("images/jupiter.jpg");
-        textures[5] = ui.loadImage("images/saturn.jpg");
-        textures[6] = ui.loadImage("images/uranus.jpg");
-        textures[7] = ui.loadImage("images/neptune.jpg");     
+        neptune = ui.loadImage("images/neptune.png"); // 35*35   
        
         halfWidth = width/2;
         halfHeight = height/2;
-
-        
+       
     }
 
-    public void render() {
-
-       //ui.translate(width/2, height/2, 0);
-        // ui.pushMatrix();        
-        // ui.translate(width/2, height/2, 0);
-        // ui.noStroke();
-        // ui.noFill();        
-        // globe = ui.createShape(SPHERE, 50);
-        // globe.setTexture(img); 
-        // ui.popMatrix();
-
-       // ui.translate(500,500, 0);
-        // ui.pushMatrix();
-        // ui.translate(750,300, 0);
-        // ui.noStroke();
-        // ui.noFill();        
-        // globe2 = ui.createShape(SPHERE, 55);
-        // globe2.setTexture(textures[0]); 
-        // ui.popMatrix();
-
+    public void render() 
+    {      
         ui.pushMatrix();
         ui.translate(halfWidth -50, halfHeight -50);
         ui.image(sun,0,0);
@@ -119,14 +82,6 @@ public class Orbits extends PlanetObjects {
         ui.rotate(NeptuneAngle);
         ui.image(neptune,-273,-273);
         ui.popMatrix();
-
-        
-
-        System.out.println(width);
-        System.out.println(height);
-
-        
-       
     }
 
     public void update()
@@ -139,16 +94,5 @@ public class Orbits extends PlanetObjects {
         SaturnAngle += 0.008;
         UranusAngle += 0.006;
         NeptuneAngle += 0.004;
-
-       // ui.shape(globe);
-      //  ui.shape(globe2);
-
-        
-       
-        
     }
 }
-   
-
-
-
---
+++ java/src/ie/tudublin/Zooming.java
@@ -0,0 +1,51 @@
+package ie.tudublin;
+
+
+import static processing.core.PConstants.SPHERE;
+
+import processing.core.PApplet;
+import processing.core.PImage;
+import processing.core.PShape;
+
+public class Zooming
+{
+
+    private float radius;
+    private PImage img;
+    private PShape globe;
+    private PShape globe2;
+    private PImage[] textures = new PImage[8];
+
+
+    img = ui.loadImage("images/sun.jpg");
+    textures[0] = ui.loadImage("images/mercury.jpg");
+    textures[1] = ui.loadImage("images/venus.jpg");
+    textures[2] = ui.loadImage("images/earth.jpg");
+    textures[3] = ui.loadImage("images/mars.jpg");
+    textures[4] = ui.loadImage("images/jupiter.jpg");
+    textures[5] = ui.loadImage("images/saturn.jpg");
+    textures[6] = ui.loadImage("images/uranus.jpg");
+    textures[7] = ui.loadImage("images/neptune.jpg");  
+
+
+
+     //ui.translate(width/2, height/2, 0);
+        // ui.pushMatrix();        
+        // ui.translate(width/2, height/2, 0);
+        // ui.noStroke();
+        // ui.noFill();        
+        // globe = ui.createShape(SPHERE, 50);
+        // globe.setTexture(img); 
+        // ui.popMatrix();
+
+       // ui.translate(500,500, 0);
+        // ui.pushMatrix();
+        // ui.translate(750,300, 0);
+        // ui.noStroke();
+        // ui.noFill();        
+        // globe2 = ui.createShape(SPHERE, 55);
+        // globe2.setTexture(textures[0]); 
+        // ui.popMatrix();
+
+
+}
